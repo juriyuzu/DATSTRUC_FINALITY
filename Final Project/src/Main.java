0ab7977bdfa481bc6e32b828f5777804d6dc76a5
juriyuzu <98145551+juriyuzu@users.noreply.github.com>
2024-05-09 15:16:59 +0800

    Add files via upload

---
+++ Final Project/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    Panel panel;
+
+    Main() {
+        setUndecorated(true);
+        setExtendedState(MAXIMIZED_BOTH);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        panel = new Panel();
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Nigga.");
+        new Main();
+    }
+}
---
+++ Final Project/src/Panel.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Panel extends JPanel {
+
+    Panel() {
+
+    }
+}
